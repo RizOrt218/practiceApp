893775aa598ee1665a75033b8a87fece3fb87287
RizOrt218 <rizhiafalen@gmail.com>
2016-03-15 22:21:58 -1000

    wip popover form

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,40 @@ angular.module('starter.controllers', [])
 .controller('createController', function($scope) {})
 
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $ionicPopover) {
+
+  var formTemplate = '<ion-popover-view><label class="item item-input"><input type="text" placeholder="Create Event Name" ng-model="event.name"></label><label class="item item-input"><input type="text" placeholder="Create Passcode" ng-model="event.passcode"></label><label class="item item-input"><span class="input-label">Start Time</span><input type="time"></label><button id="startButton" class="button button-full button-positive" ng-click=" addGroup(event.name); openModal(1);">Start!</button></ion-popover-view>';
+
+  $scope.popover = $ionicPopover.fromTemplateUrl('formTemplate', {
+    scope: $scope
+  });
+
+    $ionicPopover.fromTemplateUrl('inputCreateForm.html', {
+      scope: $scope
+    }).then(function(popover) {
+      $scope.popover = popover;
+    });
+
+
+    $scope.openPopover = function($event) {
+      $scope.popover.show($event);
+    };
+    $scope.closePopover = function() {
+      $scope.popover.hide();
+    };
+    //Cleanup the popover when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.popover.remove();
+    });
+    // Execute action on hide popover
+    $scope.$on('popover.hidden', function() {
+      // Execute action
+    });
+    // Execute action on remove popover
+    $scope.$on('popover.removed', function() {
+      // Execute action
+    });
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
